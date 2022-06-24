70bfbcf8a8a23eaddc2850852b48000a1ae371a3
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2022-06-24 23:17:30 +0100

    improved vscode testing

--- test/test.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-
-cd %~dp0..\
-rbxmk run src\main.lua test\test.rbxm
---
+++ test/test.py
@@ -0,0 +1,13 @@
+from os import path, system as cmd
+
+RBXMK_PATH = "rbxmk"
+
+SCRIPT_DIR = path.dirname(path.realpath(__file__))
+TARGET_SCRIPT = SCRIPT_DIR+"\\..\\src\\main.lua"
+MODEL_FILE = SCRIPT_DIR+"\\test.rbxm"
+
+COMMAND = f"{RBXMK_PATH} run {TARGET_SCRIPT} {MODEL_FILE}"
+
+print("files2inst main test")
+print("running: "+COMMAND)
+cmd(COMMAND)
