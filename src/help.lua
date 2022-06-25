e01f550171cf87ff792e7e986a14de2b60ed89aa
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2022-06-25 21:48:12 +0100

    updated documentation (there are no configs anymore (yet))

--- src/help.lua
+++ src/help.lua
@@ -14,8 +14,8 @@ Commands:
     help        - output basic information about the program and how to use it
 
 About configuration:
-files2inst/src contains a file called config.json, where all the settings live on:
-    "SRC_DIR": str - location to the program's source code directory
+files2inst/src contains a file called config.json, where all the settings live on
+for now, there are no settings
 ]]
 
 function module:Main()
