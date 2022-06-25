4732bd48041971d7424ca7943390ed92d039fc6a
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2022-06-25 22:16:01 +0100

    ascii art

--- src/help.lua
+++ src/help.lua
@@ -1,5 +1,15 @@
 local module = {}
 
+module.ART = [[
+  __ _ _           ___  _           _         **##__
+ / _(_) |         |__ \(_)         | |       ##     *##__
+| |_ _| | ___  ___   ) |_ _ __  ___| |_     ##   __     ##
+|  _| | |/ _ \/ __| / /| | '_ \/ __| __|   ##    *#    ##
+| | | | |  __/\__ \/ /_| | | | \__ \ |_    #___       ## 
+|_| |_|_|\___||___/____|_|_| |_|___/\__|      ***#▃_##
+                                                    ▔
+]]
+
 module.MSG = [[
 FILES2INST
 files2inst is a tool that creates an rbxm file from lua scripts and json files.
@@ -19,6 +29,7 @@ for now, there are no settings
 ]]
 
 function module:Main()
+    print(module.ART)
     print(module.MSG)
 end
 
