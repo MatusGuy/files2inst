c03ff204fc4f523fd0d5ed36ad6cafa38a3c3fca
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2022-06-25 15:33:44 +0100

    test.py support for multiple parameters

--- test/test.py
+++ test/test.py
@@ -2,15 +2,18 @@ from sys import argv as args
 from os import path, system as cmd
 
 COMMAND = args[1]
+command_args = args[2:]
+str_args = ""
+for i, arg in enumerate(command_args):
+    str_args += arg+(";" if not i == len(command_args)-1 else "")
 
 RBXMK_PATH = "rbxmk"
 
 SCRIPT_DIR = path.dirname(path.realpath(__file__))
 TARGET_SCRIPT = SCRIPT_DIR+"\\..\\src\\main.lua"
-MODEL_FILE = SCRIPT_DIR+"\\test.rbxm"
 
-RUN = f"{RBXMK_PATH} run {TARGET_SCRIPT} {COMMAND};{MODEL_FILE}"
+RUN = f"{RBXMK_PATH} run {TARGET_SCRIPT} {COMMAND};{str_args}"
 
-print("files2inst main test")
+print(f"files2inst {COMMAND} test")
 print("running: "+RUN)
 cmd(RUN)
