da6b349f91d58870014f30e62fd7403cc402af2e
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2022-06-25 10:11:27 +0100

    tree command actually working well

--- src/tree.lua
+++ src/tree.lua
@@ -1,14 +1,30 @@
 local module = {}
 
+function module:MultiplyString(str, exp)
+    for i = 1,exp,1 do
+        if i > 1 then
+            str = str..str
+        end
+    end
+    return str
+end
+
+local depth = 0 -- please don't change
 function module:PrintChildren(instance, prefix)
     prefix = prefix or ""
 
-    for i,v in ipairs(instance:GetChildren()) do
+    depth = depth + 1
+    for _,v in ipairs(instance:GetChildren()) do
         print(prefix..v.Name)
+
+        local olddepth = depth
         if #v:GetChildren() > 0 then
-            module:PrintChildren(v, "-")
+            module:PrintChildren(v, module:MultiplyString("-", depth))
         end
+        depth = olddepth
     end
+
+    depth = 0
 end
 
 -- autodetects file format, reads file and returns it
