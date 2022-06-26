98567b6b92e6676d7b479fa5356b4b2cd124b665
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2022-06-26 10:11:05 +0100

    import command weirdly doesn't import properties which makes studio pick default properties
    the part also gains surface for some reason

--- src/import.lua
+++ src/import.lua
@@ -1,24 +1,44 @@
 local module = {}
 
-function module:Import(dir, parent)
+function module:LoadJsonInstance(json)
+    local data = fs.read(json, "json")
+
+    local instance = Instance.new(data.ClassName)
+    for field, property in pairs(instance[sym.Properties]) do
+        instance[field] = property
+    end
+    
+    return instance
+end
+
+function module:Import(dir, out, parent)
     local content = fs.dir(dir)
 
-    for i, path in ipairs(content) do
-        local pathname = path.Name
-        local data = fs.read(pathname, "json")
-
-        local instance = Instance.new(data.ClassName, parent)
-        for k,v in pairs(instance[sym.Properties]) do
-            instance[k] = v
+    local instance
+    for i, childpath in ipairs(content) do
+        local pathname = childpath.Name
+        if not childpath.IsDir then
+            instance = module:LoadJsonInstance(pathname)
+            instance.Parent = parent
+        else
+            local parentInstance = module:LoadJsonInstance(path.split(pathname, "base")..".json")
+            module:Import(pathname, out, parentInstance)
         end
     end
+
+    if not parent then
+        fs.write(out, instance, "rbxm")
+    end
+    
+    return instance
 end
 
 function module:Main(args, config)
     -- args[1] - input dir
     -- args[2] - output model file
 
-    module:Import(args[2])
+    local instance = module:LoadJsonInstance(args[1]..path.split(args[1], "fstem")..".json")
+    fs.write(args[2], instance, "rbxmx")
 end
 
 return module
